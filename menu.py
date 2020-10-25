a16b51362e0159ef2c7ceec714f66995d4a8f94c
michelle frankfort <j.s.frankfort@gmail.com>
2020-10-25 09:03:50 +0100

    Added help in the menu for future use

--- menu.py
+++ menu.py
@@ -5,6 +5,7 @@ class Menu():
         print (30 * "-" , "Text based adventure" , 30 * "-")
         print ("1. start adventure (intro)")
         print ("2. start adventure (no intro)")
-        print ("3. quit")
+        print ("3. help")
+        print ("4. quit")
         print (67 * "-")
         print(" ")
