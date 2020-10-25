518d49af01be679195286ec8b1cf28bdc9f08d56
michelle frankfort <j.s.frankfort@gmail.com>
2020-10-25 09:03:12 +0100

    Finished part 0

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ console = Console()
 player = Player()
 start = True
 name = " "
-trigger = 0
 start_sim = " "
+enter = " "
 retry_input = " "
 bye = 0
 
 # Function to exit the game
 def quit():
-    print("okay bye")
+    print("Okay bye")
     exit(0)
 
 # put the intro here
@@ -22,70 +22,93 @@ def intro():
     print("please put intro here")
     adventure()
 
+def help():
+    print(" ")
+    print(67 * "-")
+    print(" ")
+    console.check_answer("Plz press enter to continue.")
+
+# Start part 0
 # Start adventure
 def adventure():
-    input("@: Is anybody there???: ")
-    time.sleep(0.5)
+    print("@: Is anybody there???: ")
+    console.check_answer("Plz press enter to continue.")
+    time.sleep(1)
     print("@: WHAAAAAA!!!!")
-    time.sleep(0.5)
-    print("@: Oh sorry. I didn't expect that you where there.")
-    time.sleep(0.5)
+    time.sleep(1)
+    print("@: Oh sorry. I didn't expect that you where here already.")
+    time.sleep(1)
     player.set_name(console.check_answer("So what is our name user?"))
     print("Oh hi " + player.get_name(name) + ". It's nice to meet you.")
-    time.sleep(0.5)
+    time.sleep(1)
     print("@: I'm escape simulator.")
-    time.sleep(0.5)
-    print("@: I'm here to teach you how it is to escape")
-    time.sleep(0.5)
+    time.sleep(1)
+    print("@: I'm here to teach you how it is to escape the country you currently live in")
+    time.sleep(1)
     print("@: So shall we start the simulator?")
-    time.sleep(0.5)
+    time.sleep(1)
     print("1. Yes")
-    time.sleep(0.5)
+    time.sleep(1)
     print("2. No")
-    time.sleep(0.5)
+    time.sleep(1)
     print(" ")
     print(67 * "-")
     print(" ")
     run()
 
 def run():
+    print(" ")
+    time.sleep(1)
     start_sim = console.check_answer("Will you start the sim?: ", ["1", "yes", "y", "2", "no", "n"]).lower()
     if start_sim == ("1") or start_sim == ("yes") or start_sim == ("y"):
         print("Nyaaa")
         Sim_start()
     elif start_sim == ("2") or start_sim == ("no") or start_sim == ("n"):
         print("@: Uhmmm...")
+        time.sleep(1)
         print("@: Are you sure?")
+        time.sleep(0.5)
         print("1. Yes")
+        time.sleep(0.5)
         print("2. No")
+        time.sleep(0.5)
         print(67 * "-")
         print(" ")
         run2()
 
 def run2():
-    sure = input("Are you sure?: ").lower()
+    sure = console.check_answer("Are you sure?: ", ["1", "yes", "y", "2", "no", "n"]).lower()
     if sure == ("1") or sure == ("yes") or sure == ("y"):
         print("@: Ohhh bye then")
+        time.sleep(0.5)
         print("You left the simulator alone without going on the adventure.")
+        time.sleep(0.5)
         print("You never know what would have happened")
+        time.sleep(0.5)
         print(67 * "-")
         print(" ")
         print("GAME OVER")
         print(" ")
+        time.sleep(1)
         print("ENDING 1")
         print("Nope not going to do this")
+        time.sleep(1)
+        console.check_answer("Plz press enter to continue.")
         play_again()
     elif sure == ("2") or sure == ("no") or sure == ("n"):
-        time.sleep(1)
         retry()
-    else:
-        print("Plz enter valid answer")
-
 
 def retry():
+    print(" ")
+    print(67 * "-")
+    print(" ")
+    time.sleep(1)
     print("So do you want to start the simulator?")
+    time.sleep(0.5)
     print("1. yes")
+    time.sleep(0.5)
     print("2. no")
+    time.sleep(0.5)
     retry_input = console.check_answer("start simulator?: ", ["1", "2", "yes", "y", "no", "n"]).lower()
     if retry_input == ("1") or retry_input == ("yes") or retry_input == ("y"):
         print(" ")
@@ -94,31 +117,47 @@ def retry():
         print("@: ARE")
         time.sleep(0.5)
         print("@: YOU")
-        time.sleep(0.5)
+        time.sleep(0.3)
         print("@: KIDDING")
-        time.sleep(0.5)
+        time.sleep(0.2)
         print("@: ME!!!!!!!")
         time.sleep(3)
         print("Good job you made the program so pissed that it blow it self up.")
+        time.sleep(1)
         print("Now you can't enter the simulator anymore.")
+        time.sleep(1)
         print("GG player")
+        time.sleep(1)
         print(67 * "-")
         print(" ")
         print("GAME OVER")
+        time.sleep(1)
         print(" ")
         print("ENDING 2")
         print("BYE BYE SIMULATOR")
+        time.sleep(1)
+        console.check_answer("Plz press enter to continue.")
         play_again()
 
+# End part 0
+# Start part 1
 def Sim_start():
-    print("hi")
+    print("")
+
 
 def play_again():
-    print("Do you want to restart the adventure or go back to menu?")
-    print("1. Yes")
-    print("2. No")
+    print(" ")
+    print(67 * "-")
+    print(" ")
+    print("Do you want to restart the adventure, go back to the menu or quit the game?")
+    time.sleep(1)
+    print("1. Restart")
+    time.sleep(1)
+    print("2. Quit")
+    time.sleep(1)
     print("3. Menu")
-    again = input("Restart, quit or menu: ").lower()
+    time.sleep(1)
+    again = console.check_answer("Restart, quit or menu: ", ["1", "yes", "y", "restart", "2", "no", "n", "quit", "3", "menu", "m"]).lower()
     if again == ("1") or again == ("yes") or again == ("y") or again == ("restart"):
         print("Okay")
         print("Good luck on your next run")
@@ -126,22 +165,22 @@ def play_again():
     elif again == ("2") or again == ("no") or again == ("n") or again == ("quit"):
         quit()
     elif again == ("3") or again == ("menu") or again == ("m"):
-        print("Lets go back")
+        print("Lets go back to the menu")
+        time.sleep(1)
         print(" ")
         print(67 * "-")
         print(" ")
-    else:
-        print("Plz enter valid answer")
-        play_again()
 
 # starts the game and menu
 while start == True:
     Menu.print_menu(Menu)
-    menu = console.check_answer("What do you want to do?", ["1", "intro", "start", "2", "skip", "quit", "3"])
+    menu = console.check_answer("What do you want to do?", ["1", "intro", "start", "2", "skip", "quit", "3", "help", "4"])
     print(" ")
     if menu == ("1") or menu == ("intro") or menu == ("start"):
         intro()
     elif menu == ("2") or menu == ("skip"):
         adventure()
-    elif menu == ("quit") or menu == ("3"):
+    elif menu == ("help") or menu == ("3"):
+        help()
+    elif menu == ("quit") or menu == ("4"):
         quit()
